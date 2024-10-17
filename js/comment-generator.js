9e81cd973e889ae29a53ae1e047b5eac9afb2866
fcklck <145225498+fcklck@users.noreply.github.com>
2024-10-18 00:53:28 +0500

    Update and rename comments.js to comment-generator.js

--- js/comment-generator.js
+++ js/comment-generator.js
@@ -1,4 +1,4 @@
-import { getRandomInt, getRandomElement } from './random.js';
+import { getRandomInt, getRandomElement } from './utils.js';
 import { messages, names } from './data.js';
 export const generateComment = () => ({
   id: getRandomInt(100, 999),
