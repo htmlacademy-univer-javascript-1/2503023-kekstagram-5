9a956afb2bfe15ba9eb7b9a8b50b4a3d52aec6bd
fcklck <145225498+fcklck@users.noreply.github.com>
2024-12-28 13:23:25 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,1 +1,15 @@
+export function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function debounce(callback, delay) {
+  let timeout;
+  return (...rest) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback.apply(this, rest), delay);
+  };
+}
